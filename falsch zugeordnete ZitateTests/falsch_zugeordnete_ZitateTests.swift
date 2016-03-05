c360a9f7a011390b43ebd2bbe90a66ff14ac9795
Paul312 <paulhuebner98@gmx.de>
2016-03-05 20:53:27 +0100

    Initial Commit

---
+++ falsch zugeordnete ZitateTests/falsch_zugeordnete_ZitateTests.swift
@@ -0,0 +1,36 @@
+//
+//  falsch_zugeordnete_ZitateTests.swift
+//  falsch zugeordnete ZitateTests
+//
+//  Created by Paul Huebner on 05.03.16.
+//  Copyright © 2016 Paul Huebner. All rights reserved.
+//
+
+import XCTest
+@testable import falsch_zugeordnete_Zitate
+
+class falsch_zugeordnete_ZitateTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
